0466432833034ef9078f29c386ebe177b5dc0fba
modalaigh <michealjodalaigh@gmail.com>
2023-08-16 20:44:12 +0100

    added changes from previous (now defunct) fork

--- ui.R
+++ ui.R
@@ -5,6 +5,8 @@ library(shinycssloaders)
 
 int_df <- readRDS('www/int_df_h_d0.Rds')
 
+help <- readRDS("www/help.Rds") # Read in list of issues for help section
+issues <- help$title
 
 shinyUI(fluidPage(
   tags$head(
@@ -16,7 +18,7 @@ shinyUI(fluidPage(
                       tags$div(class="flex-child", tags$h1("HSC Interactome"),
                                tags$p(tags$b('Explore cell-cell interactions between hematopoietic stem cells and other bone marrow cell types.', style = "font-size:18px;")),
                                tags$p(tags$b('Citation:'), 'Cell-cell interactome of the hematopoietic niche and its changes in acute myeloid leukemia.', tags$i("Ennis S et. al.,", 'iScience, 2023. DOI:', tags$a('10.1016/j.isci.2023.106943', href = 'https://doi.org/10.1016/j.isci.2023.106943')), style = "font-size:16px;"),
-                               tags$p(tags$b('Created by: '), tags$a(' Sarah Ennis', href='mailto:s.ennis6@nuigalway.ie', .noWS='outside'), ', Micheál Ó Dálaigh and',tags$a('  Jacopo Umberto Verga', href='mailto:j.verga1@universityofgalway.ie', .noWS='outside'), style = "font-size:16px;", .noWS = c("after-begin", "before-end")))),
+                               tags$p(tags$b('Created by: '), tags$a(' Sarah Ennis', href='mailto:s.ennis6@nuigalway.ie', .noWS='outside'), ',', tags$a(' Micheál Ó Dálaigh', href='mailto:m.odalaigh1@universityofgalway.ie', .noWS='outside'), ', and',tags$a('  Jacopo Umberto Verga', href='mailto:j.verga1@universityofgalway.ie', .noWS='outside'), style = "font-size:16px;", .noWS = c("after-begin", "before-end")))),
              windowTitle = 'HSC Interactome'),
   tabsetPanel(
     tabPanel('Table',
@@ -71,6 +73,21 @@ shinyUI(fluidPage(
                                       withSpinner(plotOutput('gene_plot', width = '100%', height = "600px"), color="#9FDC93", size = 0.5)
                      )
                    )))
+    ),
+    tabPanel('Help',
+             tags$br(),
+             tags$ul(
+               tags$li('Welcome to the help section of the HSC Interactome R Shiny app', style = "font-size:18px;"),
+               tags$li('Please see the drop-down list below for common questions regarding the dataset and how to navigate the app.', style = "font-size:18px;"),
+               tags$li('If your question is not answered here, please open an issue on our GitHub issues page ', tags$a('here', href = 'https://github.com/SzegezdiLab/HSC_Interactome/issues'), style = "font-size:18px;")
+             ),
+             
+             selectInput("help", label = h3("Please select an issue:", style = "font-weight: bold;"),
+                         choices = issues),
+             
+             # Render help section based on issue chosen
+             tags$h3(textOutput("help_issue"), style = "font-weight: bold;"),
+             tags$div(textOutput("help_comment"), style = "font-size: 18px;")
     )
   )
 ))
